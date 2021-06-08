316608c0c7c212b050383b7196f8ad9b5d771f1b
gagolews <m.gagolewski@gmail.com>
2021-06-08 19:24:09 +1000

    update tests/realtest.R

--- tests/realtest.R
+++ tests/realtest.R
@@ -3,10 +3,12 @@
 
 # Runs all unit tests for the package
 
+this_package <- "stringx"
+
 set.seed(123)
-library("stringx")
+library(this_package, character.only=TRUE)
 if (require("realtest", quietly=TRUE)) {
-    f <- file.path(path.package("stringx"), "realtest")
+    f <- file.path(path.package(this_package), "realtest")
     r <- test_dir(f, ".*\\.R$")
     s <- summary(r)
     print(s)
