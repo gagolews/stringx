7be21c794b289a8f78d9f1182262816d688d2c15
gagolews <m.gagolewski@gmail.com>
2021-08-12 11:51:17 +1000

    added tests (currently failing) for #7

--- inst/realtest/strptime-stringx.R
+++ inst/realtest/strptime-stringx.R
@@ -49,3 +49,51 @@ E(
     better=`attributes<-`(c("1603", "1602"), attributes(x)),
     `attributes<-`(c("1603", "1602"), list(names=names(x)))
 )
+
+
+# Testing date conversion in different timezones
+times <- list(
+    ISOdate(1970, 1, 1),  # POSIXct
+    "1970-01-01",
+    as.POSIXlt(ISOdate(1970, 1, 1)),
+    as.POSIXlt("1970-01-01"),
+    as.Date("1970-01-01"),
+    as.POSIXlt(as.Date("1970-01-01")),
+    ISOdatetime(1970, 1, 1, 12, 0, 0),  # POSIXct
+    ISOdatetime(1970, 1, 1, 23, 59, 59),
+    ISOdatetime(1970, 1, 1, 0, 0, 0),
+    base::strptime("1970-01-01 12:00:00", "%Y-%M-%D %h:%m:%s"),  # POSIXlt
+    base::strptime("1970-01-01 23:59:59", "%Y-%M-%D %h:%m:%s"),
+    base::strptime("1970-01-01 00:00:00", "%Y-%M-%D %h:%m:%s"),
+    "1970-01-01 12:00:00",
+    "1970-01-01 23:59:59",
+    "1970-01-01 00:00:00"
+)
+oldtz <- stringi::stri_timezone_get()
+
+stringi::stri_timezone_set("Etc/GMT-14")
+for (time in times)
+    E(strftime(time, "%Y-%m-%d"), "1970-01-01")
+
+stringi::stri_timezone_set("Etc/GMT+12")
+for (time in times)
+    E(strftime(time, "%Y-%m-%d"), "1970-01-01")
+
+stringi::stri_timezone_set("UTC")
+for (time in times)
+    E(strftime(time, "%Y-%m-%d"), "1970-01-01")
+
+stringi::stri_timezone_set("Australia/Melbourne")
+for (time in times)
+    E(strftime(time, "%Y-%m-%d"), "1970-01-01")
+
+stringi::stri_timezone_set("Europe/Warsaw")
+for (time in times)
+    E(strftime(time, "%Y-%m-%d"), "1970-01-01")
+
+stringi::stri_timezone_set("America/Montreal")
+for (time in times)
+    E(strftime(time, "%Y-%m-%d"), "1970-01-01")
+
+
+stringi::stri_timezone_set(oldtz)
